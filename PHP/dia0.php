f83e411a31864271469823308ca747129bf2bf3d
KauanSASobrinho <kauansousaafonso22@gmail.com>
2024-02-28 23:11:43 -0300

    dia 0

---
+++ PHP/dia0.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dia 0</title>
+</head>
+<body>
+    <h1>Calculadora xexelenta</h1><br>
+    <form method="post">
+        <input type="text" name="num1" id="num1">
+        <select name="operacao" id="operacao">
+            <option value="1">+</option>
+            <option value="2">-</option>
+            <option value="3">X</option>
+            <option value="4">/</option>
+        </select>
+        <input type="text" name="num2" id="num2">
+        <button type="submit">Calcular</button><br>
+        <input type="text" name="resultado" id="resultado">
+    </form>
+</body>
+</html>
---
+++ PHP/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto On Fire - PHP</title>
+</head>
+<body>
+    <h1>Projeto On Fire - PHP</h1><br>
+    <a href="dia0.php">Dia 0</a><br>
+</body>
+</html>
