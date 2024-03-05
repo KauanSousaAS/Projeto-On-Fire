a5a159e771a7fa41faa0eb8800dd62b36f9c8985
KauanSASobrinho <kauansousaafonso22@gmail.com>
2024-03-04 23:51:08 -0300

    dia 2

--- PHP/dia0.php
+++ PHP/dia0.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dia 0</title>
+    <title>Dia 0 - PHP</title>
 </head>
 <body>
     <h1>Calculadora xexelenta</h1><br>
--- PHP/dia1.php
+++ PHP/dia1.php
@@ -19,7 +19,7 @@ if(isset($_POST)){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dia 0</title>
+    <title>Dia 1 - PHP</title>
 </head>
 <body>
     <h1>Calculadora xexelenta</h1><br>
