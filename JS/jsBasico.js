82354c847088a9b620d160c38d679f0d0addb403
KauanSASobrinho <kauansousaafonso22@gmail.com>
2024-04-05 23:40:46 -0300

    aulas js

---
+++ JS/jsBasico.js
@@ -0,0 +1,21 @@
+// 
+console.log('Hello "World"');
+console.log("Hello 'World'");
+console.log('Hello World');
+
+console.log('Meu nome é "Kauan". Estou aprendendo JavaScript às',22,'h',30);
+// Concatenar por virgula atribui um espaço
+console.log('Meu nome é "Kauan". Estou aprendendo JavaScript às ' + 22 + 'h' + 30);
+
+// Comentário em linha
+
+console.log('Hello World'); // Pode ser inserido após um comando
+
+/*
+Comentário em bloco
+123
+321
+231
+*/
+
+// Utilizando JS dentro do HTML = jsBasico.html; row -
